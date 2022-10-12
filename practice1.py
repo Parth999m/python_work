3124678c8f4eb448046d840e755bd51b7ea83cfa
Parth999m <109942537+Parth999m@users.noreply.github.com>
2022-10-12 12:25:24 +0530

    Add files via upload

---
+++ practice1.py
@@ -0,0 +1,20 @@
+# def print_a_number(num):
+#     return(num)
+# def return_a_number(num):
+#     return num
+# def add_a_number(num):
+#     return num+3
+
+# f1 = print_a_number(7)
+# f2 = return_a_number(2)
+# print (f1)
+# print (f2)
+# f3 = add_a_number(f2)
+# print (f3)
+# f4 = add_a_number(f1)
+# print(f4)
+
+# x=0
+# while x<20:
+#     x=x+3
+# print(x) 
